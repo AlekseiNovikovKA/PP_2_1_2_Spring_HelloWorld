48536fa45e73ca3667d89a9803ccf42bd6e37f38
AlekseiNovikovKA <Lex123Lex@@>
2022-05-31 11:17:50 +0500

    Задача решена

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,22 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+
+        Cat catFirst = (Cat) applicationContext.getBean("cat");
+        Cat catSecond = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorld bean: " + (bean == beanTwo));
+        System.out.println("Cat bean: " + (catFirst == catSecond));
     }
 }
